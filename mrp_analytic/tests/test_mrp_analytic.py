3be8a8889c9fbeb87702327b3dc0dae0e94d96a9
sergiocorato <sergiocorato@gmail.com>
2020-09-15 14:55:26 +0200

    [12.0][IMP] missing account analytic in MO created from SO

--- mrp_analytic/tests/test_mrp_analytic.py
+++ mrp_analytic/tests/test_mrp_analytic.py
@@ -6,25 +6,52 @@ from odoo.tests import common
 
 class TestMrpAnalytic(common.TransactionCase):
 
-    def setUp(self):
-        super(TestMrpAnalytic, self).setUp()
-        self.analytic_account = self.env['account.analytic.account'].create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.analytic_account = cls.env['account.analytic.account'].create(
             {'name': 'Analytic account test'})
-        self.product = self.env['product.product'].create({
+        cls.product = cls.env['product.product'].create({
             'name': 'Test product',
         })
-        self.bom = self.env['mrp.bom'].create(
+        cls.bom = cls.env['mrp.bom'].create(
             {
-                'product_id': self.product.id,
-                'product_tmpl_id': self.product.product_tmpl_id.id,
+                'product_id': cls.product.id,
+                'product_tmpl_id': cls.product.product_tmpl_id.id,
             })
-        self.production = self.env['mrp.production'].create(
+        cls.production = cls.env['mrp.production'].create(
             {
-                'product_id': self.product.id,
-                'analytic_account_id': self.analytic_account.id,
-                'product_uom_id': self.product.uom_id.id,
-                'bom_id': self.bom.id,
+                'product_id': cls.product.id,
+                'analytic_account_id': cls.analytic_account.id,
+                'product_uom_id': cls.product.uom_id.id,
+                'bom_id': cls.bom.id,
             })
+        cls.partner = cls.env.ref("base.res_partner_1")
 
     def test_num_productions(self):
         self.assertEqual(self.analytic_account.num_productions, 1)
+
+    def test_sale_analytic(self):
+        module = self.env['ir.module.module'].search(
+            [('name', '=', 'sale_management')])
+        if not module:
+            return False
+        module.button_install()
+
+        sale_order = self.env['sale.order'].create({
+            'partner_id': self.partner.id,
+            'analytic_account_id': self.analytic_account.id,
+            'order_line': [
+                (0, 0, {
+                    'product_id': self.product.id,
+                    'product_uom_qty': 1,
+                    'price_unit': self.product.list_price,
+                    'name': self.product.name,
+                })
+            ],
+            'picking_policy': 'direct',
+        })
+        sale_order.with_context(test_enabled=True).action_confirm()
+        manufacturing_order = self.env['mrp.production'].search(
+            [('account_analytic_id', '=', self.analytic_account.id)])
+        self.assertTrue(manufacturing_order)
